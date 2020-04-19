d91bc63b6cdbb3938609c4223738332802c65a50
Langrzyk <betina.langrzyk@gmail.com>
2020-04-19 17:28:24 +0200

    Closure

--- Closure.py
+++ Closure.py
@@ -40,3 +40,26 @@ def mnozenie_przez(mnoznik):
 iloczyn_5_przez = mnozenie_przez(5)
 
 print(iloczyn_5_przez(12))	# zostanie wypisane 12*5, czyli 60
+
+print()
+# PRZYKŁAD 4 -------------------------------------------------------------------
+print('PRZYKŁAD 4'.center(85,'-'))
+def f(x):
+    def g(y):
+        return x + y
+    return g  # Return a closure.
+
+def h(x):
+    return lambda y: x + y  # Return a closure.
+
+# Assigning specific closures to variables.
+a = f(1)
+b = h(1)
+
+# Using the closures stored in variables.
+assert a(5) == 6
+assert b(5) == 6
+
+# Using closures without binding them to variables first.
+assert f(1)(5) == 6  # f(1) is the closure.
+assert h(1)(5) == 6  # h(1) is the closure.
