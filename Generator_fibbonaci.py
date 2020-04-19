cdecc15220877ed4fa468c499e857f2118ea99cc
Langrzyk <betina.langrzyk@gmail.com>
2020-04-19 15:49:42 +0200

    Generatory

---
+++ Generator_fibbonaci.py
@@ -0,0 +1,57 @@
+print()
+# wersja obiektowa -------------------------------------------------------------
+# bezpośredniej implementacji protokołu iteratora ------------------------------
+print('WERSJA OBIEKTOWA'.center(85,'-'))
+class Fib:
+	def __init__(self, nn):
+		self.__n = nn
+		self.__i = 0
+		self.__p1 = self.__p2 = 1
+
+	def __iter__(self):
+		#print("Fib iter")
+		return self
+
+	def __next__(self):
+		self.__i += 1
+		if self.__i > self.__n:
+			raise StopIteration
+		if self.__i in [1, 2]:
+			return 1
+		ret = self.__p1 + self.__p2
+		self.__p1, self.__p2 = self.__p2, ret
+		return ret
+
+class Class:
+	def __init__(self, n):
+		self.__iter = Fib(n)
+
+	def __iter__(self):
+		#print("Class iter")
+		return self.__iter;
+
+
+for i in Class(10):
+	print(i, end=' ')
+print()
+print(list(Class(10)))
+
+
+
+print()
+# GENERATOR Z YIELD ------------------------------------------------------------
+print('GENERATOR Z YIELD'.center(85,'-'))
+def Fibs(n):
+    p = pp = 1
+    for i in range(n):
+        if i in [0, 1]:
+            yield 1
+        else:
+            n = p + pp
+            pp, p = p, n
+            yield n
+
+for i in Fibs(10):
+	print(i, end=' ')
+print()
+print(list(Fibs(10)))
